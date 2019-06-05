df88c29684cddc22626dcbfc18b94bc6d6e8fb4c
Dmitriy <4132tester@gmail.com>
2019-06-05 10:31:13 +0300

    Добавлена функция "distance" для вычисления расстояние между двумя точками

--- sandbox/src/main/java/first/test/MyFirstProgram.java
+++ sandbox/src/main/java/first/test/MyFirstProgram.java
@@ -9,6 +9,10 @@ public class MyFirstProgram {
 
     Rectangle r = new Rectangle(3, 4);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+
+    Point a = new Point(0,0);
+    Point b = new Point(3,4);
+    System.out.println("Расстояние между двумя точками А и В равно: " + distance(a,b));
   }
 
   public static double area(Circle c) {
@@ -16,5 +20,11 @@ public class MyFirstProgram {
   }
   public static double area(Rectangle r) {
     return r.a * r.b;
+  }
+
+  public static double distance(Point a, Point b){
+    double cathetus1 = a.x - b.x;
+    double cathetus2 = a.y - b.y;
+    return Math.sqrt(cathetus1 * cathetus1 + cathetus2 * cathetus2);
   }
 }
