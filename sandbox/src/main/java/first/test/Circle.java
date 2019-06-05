3028d76c27fb198939d53fbafdcd9bd1e5ddcc89
Dmitriy <4132tester@gmail.com>
2019-06-05 12:16:50 +0300

    Функции площади пересены в классы Circle Rectangle и изменены на НЕ static

--- sandbox/src/main/java/first/test/Circle.java
+++ sandbox/src/main/java/first/test/Circle.java
@@ -7,4 +7,8 @@ public class Circle {
   public Circle (double r){
     this.r = r;
   }
+
+  public double area() {
+    return 3.1416 * this.r * this.r / 2;
+  }
 }
--- sandbox/src/main/java/first/test/MyFirstProgram.java
+++ sandbox/src/main/java/first/test/MyFirstProgram.java
@@ -4,22 +4,15 @@ package first.test;
   public static void main(String[] args) {
     System.out.println("Hello world!\n");
 
-    Circle c = new Circle(3.1416);
-    System.out.println("Площадь круга с раудисом " + c.r + " равна " + area(c));
+    Circle c = new Circle(3);
+    System.out.println("Площадь круга с раудисом " + c.r + " равна " + c.area());
 
     Rectangle r = new Rectangle(3, 4);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
 
     Point a = new Point(0,0);
     Point b = new Point(3,4);
     System.out.println("Расстояние между двумя точками А и В равно: " + Point.distance(a,b));
   }
 
-  public static double area(Circle c) {
-    return 3.1416 * c.r * c.r / 2;
   }
-  public static double area(Rectangle r) {
-    return r.a * r.b;
-  }
-
-}
