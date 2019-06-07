05213da14386e184501d44bd0b6eca8db941fcda
Dmitriy <4132tester@gmail.com>
2019-06-07 11:33:05 +0300

    Все неуникальные методы перенесены в новый родительский класс TestBase

--- addressbook-web-tests/src/test/java/ua/stqa/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/GroupCreationTest.java
@@ -1,36 +1,8 @@
 package ua.stqa.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-public class GroupCreationTest{
-  WebDriver driver;
-
-  @BeforeMethod
-  public void setUp() throws Exception {
-    driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.manage().window().maximize();
-
-    login("admin", "secret");
-  }
-
-  public void login(String username, String password) {
-    driver.get("http://localhost/addressbook/");
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
+public class GroupCreationTest extends TestBase {
 
   @Test
   public void testGroupCreation() {
@@ -41,34 +13,4 @@ public class GroupCreationTest{
     gotoGroupsPage();
   }
 
-  public void submitGroupCreation() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    driver.findElement(By.name("group_name"));
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  public void initGroupCreation() {
-    driver.findElement(By.name("new")).click();
-  }
-
-  public void gotoGroupsPage() {
-    driver.findElement(By.linkText("GROUPS")).click();
-  }
-
-  @AfterMethod
-  public void tearDown() {
-   driver.quit();
-  }
 }
