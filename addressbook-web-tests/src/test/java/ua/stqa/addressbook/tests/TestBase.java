d9a652aff12fdeb1c9532b03261756a5aa6e3802
Dmitriy <4132tester@gmail.com>
2019-06-11 12:36:29 +0300

    В класс ApplicationManager добавлен конструктор, а в TestBase - параметр, для выбора браузера в котором будет запускаться тест (предусмотрено 3 варианта: chromem firefox & internet explorer)

--- addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ua.stqa.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ua.stqa.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  public final ApplicationManager app = new ApplicationManager();
+  public final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod
   public void setUp() throws Exception {
