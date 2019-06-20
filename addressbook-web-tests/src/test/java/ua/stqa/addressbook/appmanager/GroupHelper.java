f283c0ef2dba47c215f0031cf832f76043b1c74d
Dmitriy <4132tester@gmail.com>
2019-06-20 18:12:15 +0300

    В класс GroupModificationTest внесены изменения (complex) для сравнения неупорядоченного множества групп после редактирования одной из них.

--- addressbook-web-tests/src/test/java/ua/stqa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/appmanager/GroupHelper.java
@@ -57,7 +57,8 @@ public class GroupHelper extends HelperBase {
     List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
     for (WebElement element: elements) {
       String name = element.getText();
-      GroupData group = new GroupData(name, null, null);
+      String identifier = element.findElement(By.tagName("input")).getAttribute("value");
+      GroupData group = new GroupData(identifier, name, null, null);
       groups.add(group);
     }
     return groups;
--- addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/GroupModificationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.stqa.addressbook.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -11,20 +12,26 @@ public class GroupModificationTest extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupsPage();
-    if (app.getGroupHelper().isThereAGroup()) {
+    if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().initGroupCreation();
       app.getGroupHelper().fillGroupForm(new GroupData("test_1", "test_1 header", "test_1 footer"));
       app.getGroupHelper().submitGroupCreation();
       app.getNavigationHelper().gotoGroupsPage();
     }
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size() - 1);
+    int groupIndex = before.size() - 1;
+    app.getGroupHelper().selectGroup(groupIndex);
     app.getGroupHelper().initGropModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("modified-name", "modified-header", "modified-footer"));
+    GroupData modifiedGroup =  new GroupData(before.get(before.size() - 1).getIdentifier(), "modified-name", "modified-header", "modified-footer");
+    app.getGroupHelper().fillGroupForm(modifiedGroup);
     app.getGroupHelper().submitGroupModification();
     app.getNavigationHelper().gotoGroupsPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(groupIndex);
+    before.add(modifiedGroup);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
