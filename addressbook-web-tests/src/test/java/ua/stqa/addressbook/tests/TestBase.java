d3d5db9a0eb69d430321c8d8b8ea6940b254ef10
Dmitriy <4132tester@gmail.com>
2019-07-09 17:14:46 +0300

    В классе TestBase изменена аннотация с  @BeforeMethod на @BeforeSuite для общего запуска всех тестов. Тесты для контактов изменены на "enabled = false"

--- addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/TestBase.java
@@ -1,20 +1,20 @@
 package ua.stqa.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ua.stqa.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  public final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  public static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
