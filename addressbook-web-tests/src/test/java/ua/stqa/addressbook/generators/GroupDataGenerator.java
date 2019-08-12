39972aadc0f3b0f9a190bf79f4903967de09edb1
Dmitriy <4132tester@gmail.com>
2019-08-12 22:09:19 +0300

    С помощью добавленной библиотеки jCommander (в билд Грэдл) внесены изменения в GroupDataGenerator для облегчения указания опций командной строки http://jcommander.org/

--- addressbook-web-tests/src/test/java/ua/stqa/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ua.stqa.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ua.stqa.addressbook.GroupData;
 
 import java.io.File;
@@ -11,18 +14,34 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-  public static void main (String[] args) throws IOException {
-    //сохраняем в переменные значения параметров, которые передаются при запуске программы
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
 
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+
+  public static void main (String[] args) throws IOException {
+
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
     //создаем группы (заданное колличество)
     List<GroupData> groups = generateGroups(count);
     //сохраняем созданные группы в файл на диске
-     save(groups, file);
+    save(groups, new File(file));
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
       groups.add(new GroupData().withName(String.format("test %s", i))
@@ -31,7 +50,7 @@ public class GroupDataGenerator {
     return groups;
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void save(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group: groups) {
       writer.write(String.format("%s,%s,%s\n", group.getName(),group.getHeader(),group.getHeader()));
