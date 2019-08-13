bf1fa38266fa37c041989e79cb1350db6ac96945
Dmitriy <4132tester@gmail.com>
2019-08-13 15:37:24 +0300

    В GroupCreationTest добавлен метод с аннотацией "DataProvider" для улучшениия видимости результатов теста при различных тестовых данных

--- addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ua/stqa/addressbook/tests/GroupCreationTest.java
@@ -1,28 +1,41 @@
 package ua.stqa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ua.stqa.addressbook.GroupData;
 import ua.stqa.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1")});
+    list.add(new Object[] {new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 2")});
+    list.add(new Object[] {new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupsPage();
     Groups before = app.groups().setGroups();
-    GroupData group = new GroupData().withName("test_1");
     app.groups().create(group);
-    Groups after = app.groups().setGroups();
     assertThat(app.groups().count(), equalTo(before.size() + 1));
 
+    Groups after = app.groups().setGroups();
     group.withIdentifier(after.stream().mapToInt((g) -> g.getIdentifier()).max().getAsInt());
     assertThat(after, equalTo(before.withAdded(group)));
   }
 
-  @Test
+  @Test(enabled = false)
   public void testBadGroupCreation() {
     app.goTo().groupsPage();
     Groups before = app.groups().setGroups();
